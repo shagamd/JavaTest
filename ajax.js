12f396d6e986b92c95b2ccdc048453d52c8488bc
shagamd <38699912+shagamd@users.noreply.github.com>
2022-06-22 20:48:51 -0500

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,18 +1,29 @@
-$.ajax({
+
+$(document).ready(function () {
+    iniciarInterfaz();
+});
+
+function iniciarInterfaz() {
+    $("#btnConsultar").on("click", consultarClientes)
+}
+
+function consultarClientes() {
+    $.ajax({
         method: "POST",
-        url: "./Peticiones",
+        url: "./ServletTest",
         data: {accion: 1}
     }).done(function (res) {
         var data = res.data;
         data.map((item) => {
             var tr = $("<tr/>");
-            tr.append($("<td/>").html(item.descripcion));
-            tr.append($("<td/>").html(item.identificacion));
-            tr.append($("<td/>").html(item.nombres));
-            tr.append($("<td/>").html(item.apellidos));
-            tr.append($("<td/>").html(item.login));
-            tr.append($("<td/>").html(item.direccion));
-            tr.append($("<td/>").html(item.emailcontacto));
-            $("#tbodyUsuarios").append(tr);
+            tr.append($("<td/>").html(item.idcliente));
+            tr.append($("<td/>").html(item.nombre));
+            tr.append($("<td/>").html(item.apellido));
+            tr.append($("<td/>").html(item.edad));
+            tr.append($("<td/>").html(item.documento));
+            tr.append($("<td/>").html(item.fecharegistro));
+            tr.append($("<td/>").html((item.estado ? "Activo" : "Inactivo")));
+            $("#tbodyClientes").append(tr);
         });
     });
+}
